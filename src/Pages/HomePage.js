158b2a2be77b59811485c703ab6d9ac501b7ccbe
Danish Ali <alidanish373@gmail.com>
2025-08-29 11:43:35 +0530

    Added Services Section

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,9 +1,11 @@
 import Header from "../components/Header/Header";
+import Services from "../components/Services/Services";
 
 const HomePage = () => {
   return (
     <div>
       <Header />
+      <Services />
     </div>
   )
 }
---
+++ src/components/Services/Services.js
@@ -0,0 +1,94 @@
+import "./Services.css";
+
+const Services = () => {
+    return (
+        <div className="services-container">
+            <div className="services-heading">
+                <h1>A Full Service Holistic <br /> <span>Bespoke & Collaborative</span> Approach.</h1>
+            </div>
+            <div className="services-inner-container">
+                <div className="services">
+                    <span>01</span>
+                    <div className="service">
+                        <h6>Digital Strategy</h6>
+                        <p>
+                            As a Top web design and development company in Delhi, we help businesses navigate digital
+                            transformations by aligning strategies with evolving technologies and consumer behaviors. Our approach
+                            turns challenges into opportunities, driving growth and enhancing your brand’s digital presence.
+                        </p>
+                    </div>
+                </div>
+                <div className="services">
+                    <span>02</span>
+                    <div className="service">
+                        <h6>Design Services</h6>
+                        <p>
+                            Our web design company creates visually striking, user-centric designs that deliver engaging experiences
+                            and lasting impressions. Focused on business growth, our web design services ensure relevance and responsiveness
+                            across all digital platforms. We are recognized among the top web design companies in Delhi for delivering
+                            innovative solutions.
+                        </p>
+                    </div>
+                </div>
+                <div className="services">
+                    <span>03</span>
+                    <div className="service">
+                        <h6>E-Commerce Solutions</h6>
+                        <p>
+                            Our website development company in Delhi specializes in crafting seamless, intuitive e-commerce solutions that
+                            engage users and drive conversions. We help businesses create compelling, user-friendly online stores that stand
+                            out and perform, making us a trusted web development company in Delhi for e-commerce needs.
+                        </p>
+                    </div>
+                </div>
+                <div className="services">
+                    <span>04</span>
+                    <div className="service">
+                        <h6>Web Development</h6>
+                        <p>
+                            As a Top web design and development company in Delhi, we help businesses navigate digital
+                            transformations by aligning strategies with evolving technologies and consumer behaviors. Our approach
+                            turns challenges into opportunities, driving growth and enhancing your brand’s digital presence.
+                        </p>
+                    </div>
+                </div>
+                <div className="services">
+                    <span>05</span>
+                    <div className="service">
+                        <h6>Web Application Development</h6>
+                        <p>
+                            Our web development company India specializes in building secure, scalable web applications that offer 
+                            flexibility and remote access. As a top web design agency in Delhi, we customize solutions to meet business 
+                            needs, enhancing both user experience and operational efficiency.
+                        </p>
+                    </div>
+                </div>
+                <div className="services">
+                    <span>06</span>
+                    <div className="service">
+                        <h6>Technology</h6>
+                        <p>
+                            We help businesses integrate cutting-edge technologies into their operations, ensuring seamless workflows 
+                            and greater efficiency. As a premier web solutions company, we incorporate AI, machine learning, and automation 
+                            tools that enhance business intelligence, improve decision-making, and optimize internal processes for faster 
+                            growth and innovation.
+                        </p>
+                    </div>
+                </div>
+                <div className="services">
+                    <span>07</span>
+                    <div className="service">
+                        <h6>Web Hosting Services</h6>
+                        <p>
+                            We provide reliable, secure, and high-performance web hosting services, including managed dedicated servers. 
+                            As a trusted website development company Delhi, we ensure your website runs smoothly, with fast load times and 
+                            maximum uptime for users, reinforcing our reputation as a premier web design and development company.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Services;
